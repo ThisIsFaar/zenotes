a8997dd3d967247bc14785f9587aff3f4055b66a
thisisfaar <thisisfarhan99@gmail.com>
2022-10-13 23:31:34 +0530

    firebase init

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFireStore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBxLdcJRw29go__38zIIVoADv3eUn9A8zo',
+  authDomain: 'zenotes-51e44.firebaseapp.com',
+  projectId: 'zenotes-51e44',
+  storageBucket: 'zenotes-51e44.appspot.com',
+  messagingSenderId: '179204111917',
+  appId: '1:179204111917:web:92df221ed9453ac399c2a4',
+  measurementId: 'G-EX50FCK3Y8',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFireStore(app);
