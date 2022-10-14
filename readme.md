f269200cd369cece8c58a8d7dfa1911f64d6ad28
thisisfaar <thisisfarhan99@gmail.com>
2022-10-14 19:20:34 +0530

    :rocket: v1 released

--- readme.md
+++ readme.md
@@ -15,6 +15,13 @@ Live Hosted At: https://zenotes.netlify.app/
 ## Environment Variables
 
 To run this project, you will need to add the following environment variables to your .env file
+VITE_apiKey= your firebase Api Key,
+VITE_authDomain= your firebase auth Domain,
+VITE_projectId= your firebase project Id,
+VITE_storageBucket= your firebase storage bucket,
+VITE_messagingSenderId= your firebase messaging senderId,
+VITE_appId= your firebase App Id,
+VITE_measurementId= your firebase measurement Id,
 
 ## Installation
 
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxLdcJRw29go__38zIIVoADv3eUn9A8zo',
-  authDomain: 'zenotes-51e44.firebaseapp.com',
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
   projectId: 'zenotes-51e44',
-  storageBucket: 'zenotes-51e44.appspot.com',
-  messagingSenderId: '179204111917',
-  appId: '1:179204111917:web:92df221ed9453ac399c2a4',
-  measurementId: 'G-EX50FCK3Y8',
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
